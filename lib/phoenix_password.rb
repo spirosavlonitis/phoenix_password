3786915c18104742ab38679ff47d0da3352e4535
Spiros Avlonitis <spirosa84@hotmail.com>
2017-06-23 23:32:37 +0300

    checkpoint

--- lib/phoenix_password.rb
+++ lib/phoenix_password.rb
@@ -455,5 +455,5 @@ class PhoenixPassword
 	end
 end
 
-PhoenixPassword.new(:restore=>true,:restore_cmb=>8,:checkpoint=>true,:check_cmb=>8,:check_fraction=>4).combinations({:piped=>false,:type=>'matching',
-:characters=>[0,1,2,3,4,5,6,7,8,9],:cmb_length=>[6,7,8]})
+PhoenixPassword.new(:restore=>true,:checkpoint=>true,:check_fraction=>2).combinations({:piped=>false,:type=>'matching',
+:characters=>[0,1,2,3,4,5,6,7,8,9,"a","b"],:cmb_length=>[6]})
