9f1dab005c86d3da63ea19bb1ad646157fc054dd
Spiros Avlonitis <spirosa84@hotmail.com>
2017-06-23 05:39:29 +0300

    rules

--- lib/phoenix_password.rb
+++ lib/phoenix_password.rb
@@ -409,5 +409,5 @@ class PhoenixPassword
 	end
 end
 
-PhoenixPassword.new({:restore=>true,:restore_cmb=>8}).combinations({:piped=>false,:type=>'matching',
-:characters=>[0,1,2,3,4,5,6,7,8,9],:cmb_length=>[6,7,8]})
+PhoenixPassword.new({:restore=>true,}).combinations({:piped=>false,:type=>'matching',
+:characters=>[0,1,2,3,4,5,6,7,8,9,"a"],:cmb_length=>[8]})
