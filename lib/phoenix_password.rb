f25c0ee68eb7826c6299326cec77578e97ff8440
Spiros Avlonitis <spirosa84@hotmail.com>
2017-06-24 05:24:30 +0300

    Removed extra code

--- lib/phoenix_password.rb
+++ lib/phoenix_password.rb
@@ -460,6 +460,3 @@ class PhoenixPassword
 		end
 	end
 end
-
-PhoenixPassword.new(:restore=>true,:restore_cmb=>8,:checkpoint=>true,:check_cmb=>8,:check_fraction=>4).combinations({:piped=>false,:type=>'matching',
-:characters=>[0,1,2,3,4,5,6,7,8,9],:cmb_length=>[6,7,8]})
